ce86c5a176faf5ac49e3b26711663cd82e28c4b0
Bahrdo <h.bahrdo@umcg.nl>
2024-01-31 22:10:02 +0100

    adjustment to habrok

--- data_preproc/data_preproc_config.py
+++ data_preproc/data_preproc_config.py
@@ -317,4 +317,4 @@ filename_weeklyct_metadata_json = 'weeklyct_metadata.json'  # meta-data of the W
 filename_weeklyct_npy = 'weeklyct.npy'  # WeeklyCT (Numpy array)
 
 # Run whole data_preproc pipeline for a small number of patients, useful for testing
-test_patients_list = os.listdir(weeklyct_dir)  # None  # ['0020715', '1737477', '2078963', '2970220', '3015476', '8476506']
+test_patients_list = None # os.listdir(weeklyct_dir)  # None  # ['0020715', '1737477', '2078963', '2970220', '3015476', '8476506']
